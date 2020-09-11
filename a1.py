6d61e20ee12a0415f01793ebb5e2ffe1b598d666
Dylan Le <dle8@u.rochester.edu>
2020-09-10 22:05:50 -0700

    Add LIS implementation in python

---
+++ a1.py
@@ -0,0 +1,33 @@
+def bs(x, nums):
+    lo = 0
+    hi = len(nums) - 1
+    while lo <= hi:
+        mid = (lo + hi) >> 1
+        if nums[mid] >= x:
+            hi = mid - 1
+        else:
+            lo = mid + 1
+    return lo
+
+def lis(nums):
+    bucket = []
+    order = []
+    for i in range(0, len(nums)):
+        pos = bs(nums[i], bucket)
+        if pos == len(bucket):
+            bucket.append(0)
+        bucket[pos] = nums[i]
+        order.append(pos)
+
+    max_len = len(bucket) - 1
+    res = []
+    for i in range(len(order) - 1, -1, -1):
+        if order[i] == max_len:
+            res.append(nums[i])
+            max_len -= 1
+
+    return res[::-1]
+
+
+nums = [int(num) for num in input().split(' ')]
+print(lis(nums))
