630d7862cc9fe78d991d245fd5d94c90caac7eae
hekl0 <sathubian1999@gmail.com>
2020-09-13 15:21:09 -0400

    Add cmt for Prolog

--- a1.pl
+++ a1.pl
@@ -1,10 +1,17 @@
 lis(In, Out) :-
+	% Produce all pair (leng, increasing sequence) with one_seq and then aggregate to find max pair
+	% and then save the result to res
 	aggregate(max(Leng,Seq), (one_seq(In, [], Seq), length(Seq, Leng)), max(_, Res)),
 	reverse(Res, Out).
  
+% Base case
 one_seq([], Current, Current).
  
+% Recursive to produce increasing sequence and save the sequence to Final
 one_seq([H | T], Current, Final) :-
+	% If current is empty, add H to be the first element
 	(Current = [], one_seq(T, [H], Final));
+	% If current isnt empty, check the last element H1, if H1 < H then add H to current
 	(Current = [H1 | _], H1 < H, one_seq(T, [H | Current], Final));
+	% SKip element H
 	one_seq(T, Current, Final).
