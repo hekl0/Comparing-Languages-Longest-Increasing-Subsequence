e4d96b64e4491856d4f086361f0c8217d8d5fe92
Dylan Le <dle8@u.rochester.edu>
2020-09-13 15:44:46 -0700

    Fix minors issues in prolog

--- a1.pl
+++ a1.pl
@@ -1,22 +1,37 @@
-% Reverse a list L and save it to R
-reverse_list(L, R) :- reverse_list(L,[],R).
-reverse_list([H],L,[H|L]) :- !.
-reverse_list([X|T],L,R) :- reverse_list(T,[X|L],R).
+reverse_list(L, R) :- 
+    reverse_list(L, [], R).
 
-lis(X, Y) :-
-	% Produce all pairs (leng, increasing sequence) with find_lis and then aggregate to 
-	% find max pair with max length and save the result to R
-	aggregate(max(Leng, S), (find_lis(X, [], S), length(S, Leng)), max(_, R)),
-	reverse_list(R, Y).
- 
-% Base case with no element
-find_lis([], X, X).
- 
-% Current element is H, and the rest is T
-find_lis([H | T], X, R) :-
-	% If current is empty, add H to be the first element
-	(X = [], find_lis(T, [H], R));
-	% If current isnt empty, check the last element H1, if H1 < H then add H to current
-	(X = [H1 | _], H1 < H, find_lis(T, [H | X], R));
-	% Else sKip element H
-	find_lis(T, X, R).
+reverse_list([H], L, [H|L]) :- 
+    !.
+
+reverse_list([X|T], L, R) :- 
+    reverse_list(T, [X|L], R).
+
+find_max(X, Y, A):- 
+    length(X, LX),
+    length(Y, LY),
+    (LX =< LY -> A = Y; A = X).
+
+lis(X, Y):-
+    find_lis(X, [], R),
+    reverse_list(R, Y).
+
+find_lis([], [], R):- 
+    R = [].
+
+find_lis([], [CH|CT], R):- 
+    R = [CH|CT].
+
+find_lis([H|T], [], R):-
+    find_lis(T, [], X), 
+    find_lis(T, [H], Y), 
+    find_max(X, Y, L), 
+    R = L.
+
+find_lis([H|T], [CH|CT], R):- 
+    H =< CH -> find_lis(T, [CH|CT], A), 
+    R = A; 
+    find_lis(T, [CH|CT], X), 
+    find_lis(T, [H, CH|CT], Y),
+    find_max(X, Y, A), 
+    R = A.
