9303cdafd76065f094427d7d859de4ca4aa32b73
Dylan Le <dle8@u.rochester.edu>
2020-09-13 12:36:26 -0700

    Add some salt to prolog

--- a1.pl
+++ a1.pl
@@ -1,10 +1,22 @@
-lis(In, Out) :-
-	aggregate(max(Leng,Seq), (one_seq(In, [], Seq), length(Seq, Leng)), max(_, Res)),
-	reverse(Res, Out).
+% Reverse a list L and save it to R
+reverse_list(L, R) :- reverse_list(L,[],R).
+reverse_list([H],L,[H|L]) :- !.
+reverse_list([X|T],L,R) :- reverse_list(T,[X|L],R).
+
+lis(X, Y) :-
+	% Produce all pairs (leng, increasing sequence) with find_lis and then aggregate to 
+	% find max pair with max length and save the result to R
+	aggregate(max(Leng, S), (find_lis(X, [], S), length(S, Leng)), max(_, R)),
+	reverse_list(R, Y).
  
-one_seq([], Current, Current).
+% Base case
+find_lis([], X, X).
  
-one_seq([H | T], Current, Final) :-
-	(Current = [], one_seq(T, [H], Final));
-	(Current = [H1 | _], H1 < H, one_seq(T, [H | Current], Final));
-	one_seq(T, Current, Final).
+% Current element is H, and the rest is T
+find_lis([H | T], X, R) :-
+	% If current is empty, add H to be the first element
+	(X = [], find_lis(T, [H], R));
+	% If current isnt empty, check the last element H1, if H1 < H then add H to current
+	(X = [H1 | _], H1 < H, find_lis(T, [H | X], R));
+	% Else sKip element H
+	find_lis(T, X, R).
