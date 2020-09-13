88403a9537dc803712dddeacfa21b292148008a3
Dylan Le <dle8@u.rochester.edu>
2020-09-13 15:43:49 -0700

    Fix minors issues in prolog

---
+++ a2.pl
@@ -0,0 +1,37 @@
+reverse_list(L, R) :- 
+    reverse_list(L, [], R).
+
+reverse_list([H], L, [H|L]) :- 
+    !.
+
+reverse_list([X|T], L, R) :- 
+    reverse_list(T, [X|L], R).
+
+find_max(X, Y, A):- 
+    length(X, LX),
+    length(Y, LY),
+    (LX =< LY -> A = Y; A = X).
+
+lis(X, Y):-
+    find_lis(X, [], R),
+    reverse_list(R, Y).
+
+find_lis([], [], R):- 
+    R = [].
+
+find_lis([], [CH|CT], R):- 
+    R = [CH|CT].
+
+find_lis([H|T], [], R):-
+    find_lis(T, [], X), 
+    find_lis(T, [H], Y), 
+    find_max(X, Y, L), 
+    R = L.
+
+find_lis([H|T], [CH|CT], R):- 
+    H =< CH -> find_lis(T, [CH|CT], A), 
+    R = A; 
+    find_lis(T, [CH|CT], X), 
+    find_lis(T, [H, CH|CT], Y),
+    find_max(X, Y, A), 
+    R = A.
